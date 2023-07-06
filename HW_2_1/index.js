2108e0fa3a169a9a5d43e9ede1a98d135b94173a
Андрущенко <andruschenko033@gmail.com>
2023-07-06 20:31:18 +0300

    Changed HW_2_1

--- HW_2_1/index.js
+++ HW_2_1/index.js
@@ -4,8 +4,3 @@ var input3 = prompt("Enter the third line:");
 var result = input1 + " " + input2 + " " + input3;
 
 console.log(result);
-
-/*var number = prompt('Enter a five-digit number:');
-var numberString = number.toString();
-var digits = numberString.split('');
-console.log(digits.join(' '));*/
