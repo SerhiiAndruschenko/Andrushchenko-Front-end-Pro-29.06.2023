72b4e04437316928b1a36195a31f951b691b4a67
Андрущенко <andruschenko033@gmail.com>
2023-07-10 22:43:27 +0300

    HW_3

--- HW_3_2/index.js
+++ HW_3_2/index.js
@@ -1,7 +1,9 @@
 var number1 = +prompt('Enter the first number:');
 var number2 = +prompt('Enter the second number:');
 var number3 = +prompt('Enter the third number:');
-
-var average = (number1 + number2 + number3) / 3;
-
-alert(`Average value is ${average}`);
+if (isNaN(number1) || isNaN(number2) || isNaN(number3)) {
+  alert('Invalid input. Please enter numbers only.');
+} else {
+  var average = (number1 + number2 + number3) / 3;
+  alert(`Average value is ${average}`);
+}
