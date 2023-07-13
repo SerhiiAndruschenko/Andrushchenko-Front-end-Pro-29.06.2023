985a24e3b556bf10d65776106d3c08b48b638791
Андрущенко <andruschenko033@gmail.com>
2023-07-13 20:20:04 +0300

    HW_4

--- HW_4_1/index.js
+++ HW_4_1/index.js
@@ -1,36 +1,44 @@
 const operation = prompt("What do you want to do? (add, sub, mult, div)");
 
-if(operation != 'add' && operation != 'sub' && operation != 'mult' && operation != 'div'){
+if(operation !== 'add' && operation !== 'sub' && operation !== 'mult' && operation !== 'div'){
   alert("Error: Unknown operation!");
-} else{
-  let num1 = parseFloat(prompt("Enter the first number: "));
-  let num2 = parseFloat(prompt("Enter the second number: "));
+} else {
+  const num1 = parseFloat(prompt("Enter the first number: "));
+  const num2 = parseFloat(prompt("Enter the second number: "));
 
   let result;
   let operationSymbol;
 
-  if(operation == 'add' && !isNaN(num1) && !isNaN(num2)){
-    result = num1 + num2;
-    operationSymbol = '+';
-  } else if(operation == 'sub' && !isNaN(num1) && !isNaN(num2)){
-    result = num1 - num2;
-    operationSymbol = '-';
-  } else if(operation == 'mult' && !isNaN(num1) && !isNaN(num2)){
-    result = num1 * num2;
-    operationSymbol = '*';
-  } else if(operation == 'div' && !isNaN(num1) && !isNaN(num2)){
-    if (num2 !== 0) {
-      result = num1 / num2;
-      operationSymbol = '/';
-    } else {
-      alert("Error: Division by zero!");
+  if(!isNaN(num1) && !isNaN(num2)){
+    switch(operation){
+      case 'add':
+        result = num1 + num2;
+        operationSymbol = '+';
+        break;
+      case 'sub':
+        result = num1 - num2;
+        operationSymbol = '-';
+        break;
+      case 'mult':
+        result = num1 * num2;
+        operationSymbol = '*';
+        break;
+      case 'div':
+        if (num2 !== 0) {
+          result = num1 / num2;
+          operationSymbol = '/';
+        } else {
+          alert("Error: Division by zero!");
+        }
+        break;
     }
+
+    if (result !== undefined) {
+      alert(`${num1} ${operationSymbol} ${num2} = ${result}`);
+    }
+
   } else {
     alert("Invalid input. Please enter numbers only.");
-  }
-
-  if (result !== undefined) {
-    alert(`${num1} ${operationSymbol} ${num2} = ${result}`);
   }
 
 }
