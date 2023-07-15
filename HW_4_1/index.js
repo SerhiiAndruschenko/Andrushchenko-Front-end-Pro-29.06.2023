8931399a7fb99c8c0473f862f25912ff2e36bc1e
Андрущенко <andruschenko033@gmail.com>
2023-07-15 23:19:27 +0300

    HW_4

--- HW_4_1/index.js
+++ HW_4_1/index.js
@@ -1,45 +1,34 @@
 const operation = prompt("What do you want to do? (add, sub, mult, div)");
+const num1 = +prompt("Enter the first number: ");
+const num2 = +prompt("Enter the second number: ");
+let result;
+let operationSymbol;
 
-if(operation === 'add' || operation === 'sub' || operation === 'mult' || operation === 'div'){
-  const num1 = +prompt("Enter the first number: ");
-  const num2 = +prompt("Enter the second number: ");
-
-  let result;
-  let operationSymbol;
-
-  if(!isNaN(num1) && !isNaN(num2)){
-    switch(operation){
-      case 'add':
-        result = num1 + num2;
-        operationSymbol = '+';
-        break;
-      case 'sub':
-        result = num1 - num2;
-        operationSymbol = '-';
-        break;
-      case 'mult':
-        result = num1 * num2;
-        operationSymbol = '*';
-        break;
-      case 'div':
-        if (num2 !== 0) {
-          result = num1 / num2;
-          operationSymbol = '/';
-        } else {
-          alert("Error: cannot divide by 0!");
-        }
-        break;
+switch(true){
+  case operation == 'add' && !isNaN(num1) && !isNaN(num2):
+    result = num1 + num2;
+    operationSymbol = '+';
+    break;
+  case operation == 'sub' && !isNaN(num1) && !isNaN(num2):
+    result = num1 - num2;
+    operationSymbol = '-';
+    break;
+  case operation == 'mult' && !isNaN(num1) && !isNaN(num2):
+    result = num1 * num2;
+    operationSymbol = '*';
+    break;
+  case operation == 'div' && !isNaN(num1) && !isNaN(num2):
+    if (num2 !== 0) {
+      result = num1 / num2;
+      operationSymbol = '/';
+    } else {
+      alert("Error: cannot divide by 0!");
     }
-
-    if (result !== undefined) {
-      alert(`${num1} ${operationSymbol} ${num2} = ${result}`);
-    }
-
-  } else {
-    alert("Error: Please enter numbers only.");
-  }
-  
-} else {
-  alert("Error: Wrong operation!");
+    break;
+  default:
+    alert("Error: Wrong operation!");
 }
 
+if (result !== undefined) {
+  alert(`${num1} ${operationSymbol} ${num2} = ${result}`);
+}
