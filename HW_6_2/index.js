72d3d68a9882c2b6e6a6015e37ed24e861583815
Андрущенко <andruschenko033@gmail.com>
2023-07-20 23:57:42 +0300

    HW_6_2

--- HW_6_2/index.js
+++ HW_6_2/index.js
@@ -7,12 +7,11 @@ console.log(arr.join(' '));
 
 /*2. Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів. */
 for(let i = 10; i <= 100; i += 10){
-  console.log(i * 27);
   console.log(`${i} доларів = ${i * 27} гривень`);
 }
 
 /*3. Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N. */
-let N = 100;
+let N = 200;
 for (let i = 1; i <= 100; i++) {
   if (i * i <= N) {
     console.log(i);
@@ -21,7 +20,7 @@ for (let i = 1; i <= 100; i++) {
 
 /*4. Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).*/
 let isSimple = true;
-let numberToCheck = +prompt('');
+let numberToCheck = 13;
 if(numberToCheck <= 1){
   isSimple = false;
 } else {
@@ -32,6 +31,20 @@ if(numberToCheck <= 1){
   }
 }
 let result = isSimple ? `${numberToCheck} є простим числом` : `${numberToCheck} не є простим числом`
-alert(result);
+console.log(result);
 
 /*5. Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).*/
+let number = 81;
+let found = false;
+let resultValue = 1;
+let power = 0;
+for (let i = 1; i < number; i++) {
+  resultValue *= 3;
+  power += i;
+  if (resultValue === number) {
+    found = true;
+    break;
+  }
+}
+let resulltMessage = found ? `${number} можна отримати шляхом зведення числа 3 у ступінь ${power}.` : `${number} не можна отримати шляхом зведення числа 3 у деякий ступінь.`;
+console.log(resulltMessage);
