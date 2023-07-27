73afe6fb288a639648d954fb891f848b4355a970
Андрущенко <andruschenko033@gmail.com>
2023-07-27 21:55:49 +0300

    HW_8

--- HW_7_2/index.js
+++
@@ -1,8 +0,0 @@
-function removeElement(array, item) {
-  let index = array.indexOf(item);
-  array.splice(index, 1);
-}
-
-const array = [1, 2, 3, 4, 5, 6, 7];
-removeElement(array, 5 );
-console.log(array);
