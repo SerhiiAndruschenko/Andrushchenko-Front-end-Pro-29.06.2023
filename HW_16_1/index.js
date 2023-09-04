821a221ad0db7a64fa8498130d754938344ab5d0
Андрущенко <andruschenko033@gmail.com>
2023-09-04 18:50:00 +0300

    HW_16

--- HW_16_1/index.js
+++ HW_16_1/index.js
@@ -1,37 +1,41 @@
-const citySelect = document.getElementById("citySelect");
-const temperature = document.getElementById("temp");
-const pressure = document.getElementById("pressure");
-const description = document.getElementById("description");
-const humidity = document.getElementById("humidity");
-const speed = document.getElementById("speed");
-const deg = document.getElementById("deg");
-const weatherIcon = document.getElementById("weatherIcon");
+const citySelect = document.getElementById('citySelect');
+const temperature = document.getElementById('temp');
+const pressure = document.getElementById('pressure');
+const description = document.getElementById('description');
+const humidity = document.getElementById('humidity');
+const speed = document.getElementById('speed');
+const deg = document.getElementById('deg');
+const weatherIcon = document.getElementById('weatherIcon');
+const defaultCity = 'Kyiv';
 
-citySelect.addEventListener("change", function() {
+citySelect.addEventListener('change', function() {
   let selectedCity = this.value;
-  if(selectedCity !== 'default'){
-    console.log(selectedCity);
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=metric&APPID=5d066958a60d315387d9492393935c19`;
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        temperature.textContent = data.main.temp + ' °C';
-        pressure.textContent = data.main.pressure + ' hPa';
-        description.textContent = data.weather[0].description;
-        humidity.textContent = data.main.humidity + ' %';
-        speed.textContent = data.wind.speed + ' m/s';
-        deg.textContent = data.wind.deg + ' %';
-        weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-      })
-      .catch((error) => {
-        alert("Помилка при отриманні даних про погоду.");
-      });
-  }  
+  fetchWeather(selectedCity);
 });
+
+function fetchWeather(selectedCity) {
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=metric&APPID=5d066958a60d315387d9492393935c19`;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      temperature.textContent = data.main.temp + ' °C';
+      pressure.textContent = data.main.pressure + ' hPa';
+      description.textContent = data.weather[0].description;
+      humidity.textContent = data.main.humidity + ' %';
+      speed.textContent = data.wind.speed + ' m/s';
+      deg.textContent = data.wind.deg + ' %';
+      weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    })
+    .catch((error) => {
+      alert('Помилка при отриманні даних про погоду.');
+  });
+}
+
+fetchWeather(defaultCity);
 
 //Async/await спосіб
 /*
-citySelect.addEventListener("change", function() {
+citySelect.addEventListener('change', function() {
   let selectedCity = this.value;
   if(selectedCity !== 'default'){
     weatherJSON(selectedCity).then(data => {
