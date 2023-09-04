b376d6d7b4bb497b4e266a23ef37cd853e477a0d
Андрущенко <andruschenko033@gmail.com>
2023-09-05 00:49:18 +0300

    HW_17

---
+++ HW_17_1/index.js
@@ -0,0 +1,26 @@
+function generateList(array) {
+  let html = '<ul>';
+
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      html += '<li>';
+      html += '<ul>';
+      item.forEach((subItem) => {
+        html += `<li>${subItem}</li>`;
+      });
+      html += '</ul>';
+      html += '</li>';
+    } else {
+      html += `<li>${item}</li>`;
+    }
+  });
+
+  html += '</ul>';
+  return html;
+}
+
+// Приклад використання функції:
+const array = [1, [1.1, 1.2, 1.3], 2, 3, [3.1, 3.2, 3.3], 4];
+const result = generateList(array);
+
+document.body.innerHTML = result;
--- README.md
+++
@@ -1,3 +0,0 @@
-# Andrushchenko-Front-end-Pro-29.06.2023
-
-Homework test file
